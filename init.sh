733b20a83d9bf4381188934e05e14eddc42fee43
MariaImangalina <varenik_geo@mail.ru>
2020-10-20 14:25:02 +0300

    start django

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 
 sudo /etc/init.d/nginx restart
+
+sudo gunicorn -c /home/box/etc/gunicorn-django.conf ask.wsgi:application
